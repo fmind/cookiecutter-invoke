d8a6985ea2201b15c9f22f12714d3e606961e133
Médéric Hurier <fmind@fmind.me>
2019-12-15 13:58:04 +0100

    Update {{cookiecutter.name}}.py

--- {{cookiecutter.name}}/{{cookiecutter.name}}.py
+++ {{cookiecutter.name}}/{{cookiecutter.name}}.py
@@ -9,5 +9,4 @@ def venv(c, force=False):
     """Create a virtual environment."""
     if not Path("venv").exists() or force:
         c.run("python3 -m venv venv --clear")
-    c.run("venv/bin/pip install --editable .")
-    c.run("venv/bin/pip install -r requirements.txt")
+        c.run("venv/bin/pip install -r requirements.txt")
